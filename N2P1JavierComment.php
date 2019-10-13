ab71d43f28f44b374abea4a61e2eaa54355b4ace
baltorius89 <55503390+baltorius89@users.noreply.github.com>
2019-10-13 23:25:59 +0200

    Add files via upload

---
+++ N2P1JavierComment.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "<p>\n";
+    echo "\t<ul> 
+        \t<li>Puedes jugar con las fechas</li>
+        \t<li>Puedes hacer todo tipo de calculos con las fechas</li>
+        \t<li>Puedes representar las fechas de mil formas</li>
+        \t<li>Con la cookie no hace falta que tenga que poner de nuevo todas las caracteristicas</li>
+        \t<li>El hecho de usar formularios puedes introducirle lo que quieras siempre que se lo permita el tipo de dato</li>
+        \t<li></li>
+        \t<li>El pie de pagina puedes incluirlo con mucha facilidad en cualquier fichero</li>
+        \t<li>Con el s witch no me ha hecho falta hacer tantos ifs</li>
+        \t<li>Puedo saber cuantas veces he refrescado la pagina con el is set</li>
+        \t<li>Puedo pedir lo que sea en el formulario para identificar el usuario si ha iniciado sesion bien o se ha equivocado y su cuenta no existe</li>
+        </ul>\n";  
+    echo "</p>\n";
+    echo "<p>Mi puntuacion al documento es de un 0 y la puntuacion de la explicacion del profesor un 7";
+    echo "</p>\n";
+    echo "<p>Me daria la puntuacion de un 0";
+    echo "</p>\n";
+    echo "<p>Mi profesor me ha ayudado bastante, y me ha motivado, pero en lo que respecta al documento, no he encontrado nada de lo que he usado para hacer la practica, la he tenido que sacar de internet y buscarme la vida";
+    echo "</p>";
